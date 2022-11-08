243a3d51720a8a377bd20e6a4a20d773c2cf456a
hvad <david.hannequin@gmail.com>
2022-11-08 07:21:34 +0000

    Add option for threshold.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use clap::Parser;
 use psutil::disk;
 use std::process;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
     disk: String,
+    #[arg(short, long)]
+    critical: u8,
+    #[arg(short, long)]
+    warning: u8,
 }
 
 fn main() {
@@ -16,13 +19,13 @@ fn main() {
     let disk_usage = disk::disk_usage(&args.disk).unwrap();
     let disk_usage_percent = disk_usage.percent() as u8;
 
-    if disk_usage_percent > 90 {
+    if disk_usage_percent > args.critical {
         println!(
             "CRITICAL - Disk {} usage {}%",
             &args.disk, disk_usage_percent
         );
         process::exit(2);
-    } else if disk_usage_percent > 80 {
+    } else if disk_usage_percent > args.warning {
         println!(
             "WARNING - Disk {} usage {}%",
             &args.disk, disk_usage_percent
